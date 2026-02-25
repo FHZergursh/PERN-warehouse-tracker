2a73718011738dd4a6e28a7cbc876eb94c31b53d
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-02-25 04:34:30 +0000

    simple header layout
    haven't touched this project in a bit, so I'm making this very small PR to keep it in my attention

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='bg-gray-500 py-10 flex justify-center'>
+      <div className='mx-200 bg-red-500 w-full flex justify-between'> 
+        <div>Homepage</div>
+        <div>Inventory</div>
+        <div>Orders</div>
+      </div>
+
+
+    </div>
   )
 }
 
